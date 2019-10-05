477b59f0d10ec90ddaa83333e23c6e0f31099557
erikaperugachi <erikaperugachi@gmail.com>
2019-10-05 14:31:47 -0500

    Add step 1: start with alice

--- email_mailbox/src/utils/electronEventInterface.js
+++ email_mailbox/src/utils/electronEventInterface.js
@@ -1292,6 +1292,7 @@ export const sendLoadEventsEvent = params => {
 };
 
 ipcRenderer.on('socket-message', async (ev, message) => {
+  if (needsUpgrade()) return;
   const eventId = message.cmd;
   if (eventId === 400) {
     sendLoadEventsEvent({ showNotification: true });
@@ -1720,6 +1721,7 @@ export const sendManualSyncSuccessMessage = () => {
 /*  Firebase
 ----------------------------- */
 ipcRenderer.on(NOTIFICATION_SERVICE_STARTED, async (_, token) => {
+  if (needsUpgrade()) return;
   await updatePushToken(token);
 });
 
