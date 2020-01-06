e70683a0b78cc3eb530e2db702f07948e86dedcc
erikaperugachi <erikaperugachi@gmail.com>
2020-01-06 17:03:21 -0500

    Adapt encryption database. Close #1258

--- email_mailbox/src/utils/electronEventInterface.js
+++ email_mailbox/src/utils/electronEventInterface.js
@@ -929,7 +929,7 @@ const handleEmailTrackingUpdate = async ({ rowid, params }) => {
           emailId: email.id,
           contactId
         };
-        await createFeedItem([feedItemParams]);
+        await createFeedItem(feedItemParams);
         feedItemAdded = true;
       }
     }
